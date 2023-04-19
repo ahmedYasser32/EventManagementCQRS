1dfe859d433b5c1ffdbff9fd2fc1c3d5233a540c
ahmadyasser32 <ahmed.yasser32@gmail.com>
2023-04-19 05:13:08 +0200

    domain layer

---
+++ Domain/Entities/Event.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Entities
+{
+    public class Event
+    {
+        public int Id { get; set; } 
+        public string   ArabicTitle { get; set; }
+        public string   Title { get; set; }   
+        public string   Content { get; set; }     
+        public string   Address { get; set; }    
+        public string   CoverPhotoPath { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+        public Source Source { get; set; }
+        public IEnumerable<Category> Categories { get; set; }
+
+        public PhotoAlbum PhotoAlbum { get; set; }
+
+    }
+}
---
+++ Domain/Entities/PhotoAlbum.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Entities
+{
+    public class PhotoAlbum
+    {
+        public PhotoAlbum()
+        {
+            PhotoPaths = new List<string>();
+        }
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public List<string> PhotoPaths { get; set; }
+        public IEnumerable<Event>? Events { get; set; }
+
+
+
+    }
+}
